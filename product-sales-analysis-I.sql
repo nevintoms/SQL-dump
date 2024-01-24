adc0d16c051abd7bc0744714c9eee2687e129f1f
Nevin Toms <nevintoms333@gmail.com>
2024-01-24 00:34:16 -0500

    Add solution.

--- product-sales-analysis-I.sql
+++ product-sales-analysis-I.sql
@@ -71,3 +71,5 @@ From sale_id = 2, we can conclude that Nokia was sold for 5000 in the year 2009.
 From sale_id = 7, we can conclude that Apple was sold for 9000 in the year 2011.
 */
 
+--Solution:
+select p.product_name, s.year, s.price from Sales as s inner join Product as p on p.product_id = s.product_id

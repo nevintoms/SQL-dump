1c7a048dfff099f35933f54bdbb5ac92321a4468
Nevin Toms <nevintoms333@gmail.com>
2024-01-31 17:04:18 -0500

    Add solution.

--- average-time-of-process-per-machine.sql
+++ average-time-of-process-per-machine.sql
@@ -66,3 +66,10 @@ Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
 Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
 */
 
+--Solution:
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time 
+from Activity a1
+join Activity a2 
+on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
+and a1.activity_type='start' and a2.activity_type='end'
+group by a1.machine_id

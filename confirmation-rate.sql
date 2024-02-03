6271ad8840eadad2187cf0cdb6e9a00310d48d48
Nevin Toms <nevintoms333@gmail.com>
2024-02-03 11:10:45 -0500

    Add alternate solution.

--- confirmation-rate.sql
+++ confirmation-rate.sql
@@ -84,3 +84,8 @@ from Signups as s
 left join Confirmations as c on s.user_id = c.user_id
 group by s.user_id
 
+--Alternate way:
+select s.user_id, round(avg(CASE WHEN c.action="confirmed" THEN 1 ELSE 0 END),2) as confirmation_rate
+from Signups as s
+left join Confirmations as c on s.user_id = c.user_id
+group by s.user_id

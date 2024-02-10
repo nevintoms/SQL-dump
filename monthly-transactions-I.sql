a796b8566286e83e82aa028d4af1cc5214cd643f
Nevin Toms <nevintoms333@gmail.com>
2024-02-09 23:48:31 -0500

    Add solution.

--- monthly-transactions-I.sql
+++ monthly-transactions-I.sql
@@ -46,3 +46,12 @@ Output:
 +----------+---------+-------------+----------------+--------------------+-----------------------+
 */
 
+--Solution:
+select DATE_FORMAT(trans_date, "%Y-%m") as month, 
+       country,
+       count(*) as trans_count,
+       count(if(state LIKE "approved", 1, null)) as approved_count,
+       sum(amount) as trans_total_amount,
+       sum(if(state LIKE "approved", amount, 0)) as approved_total_amount
+from Transactions
+group by DATE_FORMAT(trans_date, "%Y-%m"), country
